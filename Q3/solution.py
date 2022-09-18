5417c451d43c9e723a7c4031e7fcc58b4a70720e
Mujeeb Khalil <mujeeb.memon2k@gmail.com>
2022-09-18 19:39:45 +0500

    Update solution.py

--- Q3/solution.py
+++ Q3/solution.py
@@ -1,3 +1,10 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+def median(num1, num2):
+  
+  num1.extend(num2)   # Merging the two lists
+  num1.sort()         # Sorting the merged list
+  middle_element = len(num1) // 2     # finding the middle element 
+  if len(num1) % 2 == 0:             
+    return (num1[middle_element] + num1[middle_element- 1])/2   # returning the median of even length list
+  else:
+    return num1[middle_element]         # returning the median of odd length list
+
