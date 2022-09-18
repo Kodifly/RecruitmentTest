d369366b9d16144a27c1c60816ed9b4e0cc0b9c6
Mujeeb Khalil <mujeeb.memon2k@gmail.com>
2022-09-18 20:55:14 +0500

    Update solution.py

--- Q2/solution.py
+++ Q2/solution.py
@@ -4,5 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+  def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    #used a double pointer approach
+    fast = head         #both pointer representing the head
+    slow = head
+ 
+    for i in range(n):   #iterating the fast pointer n times
+      fast = fast.next
+            
+    if not fast:
+      return head.next
+ 
+    while fast.next:    #iterating the both pointer until fast reaches the end
+      fast = fast.next
+      slow = slow.next
+       
+    slow.next = slow.next.next  # replacing the next node of slow pointer with the next to next node.
+    return head
+
+        
         
