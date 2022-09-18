13d203e60dea6812e3da48f77d24cd235067d9ab
Hassam Ali Khan <104498954+HassamAliKhan@users.noreply.github.com>
2022-09-18 23:57:04 +0500

    Update solution.py
    Task#1 for Assessment of Kodifly.

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,1 +1,33 @@
-## Add code below with answer clearly stated
+import math
+
+
+def sumOfFac(number):
+    
+    value = []
+    value.append(1)
+
+    for i in range(1, number+1):
+        getCalcFac(value, i)
+    
+    sum = 0
+    for i in range(len(value)):
+        sum += value[i]
+    return sum
+
+
+def getCalcFac(value, j ):
+    carry = 0
+
+    for i in range(len(value)):
+        result  = carry + value[i] * j
+        value[i] = result % 10 
+        carry = result //10
+
+    while carry != 0:
+        value.append(carry % 10)
+        carry = carry // 10
+    
+
+if __name__ == "__main__":
+    print(str(math.factorial(100)))
+    print(sumOfFac(100))
