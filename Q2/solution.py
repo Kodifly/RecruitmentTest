09c524d27aa7e862bf84f144958757c5432b6df5
hiba007 <37494476+hiba007@users.noreply.github.com>
2022-09-18 21:56:45 +0500

    Update solution.py

--- Q2/solution.py
+++ Q2/solution.py
@@ -6,3 +6,30 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
+        prev_node = head
+        next_node = head
+
+        count = 0
+
+        while prev_node.next != None:
+
+            count += 1
+
+            if prev_node.next.value == value:
+                next_node = next_node.next.next
+                break
+
+            prev_node = prev_node.next
+            next_node = next_node.next
+
+        if head.value == value:
+            node = head
+            head = head.next
+            del node
+            
+        else:
+             del_node = prev_node.next
+             next_node = prev_node.next.next
+             del del_node
+            
+        return head
