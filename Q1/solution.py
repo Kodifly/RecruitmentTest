2c6a56b08e80d3e816ea9e71efd98f14ac9aa21f
hASEEBjAN <engrhaseebjan@gmail.com>
2022-09-18 23:21:47 +0500

    My answers to the assessment test

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,1 +1,24 @@
 ## Add code below with answer clearly stated
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        temp = 1
+        for i in range(1, n):
+            temp *= i
+        return temp
+
+def sum_factorial(n):
+    if n < 1:
+        print("Invalid Value!!!")
+        return None
+    if n == 1:
+        return 1
+    else:
+        fact_n = factorial(n)
+        temp = 0
+        for i in str(fact_n):
+            temp += int(i)
+        return temp
+
+print("The sum of 100! is ", sum_factorial(100))
