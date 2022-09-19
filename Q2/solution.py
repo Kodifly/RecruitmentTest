f002f053aac09e6dfe42f76ee5eaecd226ecb7a3
Umar Farooq <umar5555f@gmail.com>
2022-09-19 07:26:28 +0000

    Update solution.py
    Added the solution for the second question

--- Q2/solution.py
+++ Q2/solution.py
@@ -5,4 +5,24 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        temp = head
+        c = 0
+        while temp:                             #Find the length of the linked list
+            c+=1
+            temp = temp.next
+        a = c-n+1                               #Calculate the node to be removed
+        i = 1
+        temp2 = head
+        prev = None
+        while i < a:                            #Traverse till the node to be removed
+            i+=1
+            prev = temp2                        #Prev pointer to point the previous node of the deletion node
+            temp2 = temp2.next
+        if temp2==head:
+            return head.next
+        prev.next = temp2.next                  #Link the previous node to the next of the deletion node
+        return head
         
+
+        # I've done this problem already 0n 02/21/2022 17:58 at that time my submission was faster than 97% people at leetcode.
+        # Myprofile link : https://leetcode.com/umikhaan/
